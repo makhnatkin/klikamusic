a86497a29a6d8944e9f6604430085a00d6fd7482
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2016-12-23 19:35:56 +0300

    TASK-2: Added loadData action, added ajax-request

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import request from 'superagent-bluebird-promise';
+const musicRequest = 'https://freemusicarchive.org/featured.json';
+
+// action types
+export const LOAD_INITAIL_DATA__START = 'LOAD_INITAIL_DATA__START';
+export const LOAD_INITAIL_DATA__SUCCESS = 'LOAD_INITAIL__SUCCESS';
+export const LOAD_INITAIL_DATA__FAIL = 'LOAD_INITAIL_DATA__FAIL';
+
+// action creaters
+export const loadData = () => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: LOAD_INITAIL_DATA__START
+    });
+
+    request.get(musicRequest)
+      .then(
+        res => console.log(JSON.parse(res.text)),
+        error => console.log(error)
+      )
+  }
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
 import "../stylesheets/main.scss";
 
-// app component
-export default class App extends React.Component {
-  // render
+// Actions
+import { loadData } from '../actions';
+
+// TODO: may be transfer to components?..
+const Loader = props => <h1>Loading...</h1>;
+
+// App component
+export class App extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(loadData());
+  }
+
   render() {
+    const { music, children } = this.props;
+    if (!music.length) {
+      return (
+        <Loader />
+      );
+    }
+
     return (
       <div className="container">
-        {this.props.children}
+        {children}
       </div>
     );
   }
 }
+
+// export the connected class
+const mapStateToProps = ({ music=[] }) => ({
+  music,
+});
+
+export default connect(mapStateToProps)(App);
